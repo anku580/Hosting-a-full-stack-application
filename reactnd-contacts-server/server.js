5f0f0b9f24d91b3b697b042a7965d60df1f02ca7
anku580 <ankugarg580@gmail.com>
2021-08-29 10:42:45 +0000

    updated backend project to support APIs

--- reactnd-contacts-server/server.js
+++ reactnd-contacts-server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Sequelize, DataTypes, Model } = require("sequelize");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const config = require("./config");
@@ -9,6 +10,35 @@ const app = express();
 
 app.use(express.static("public"));
 app.use(cors());
+
+const sequelize = new Sequelize(
+  'postgres://postgres:postgres@database-2.ckumz2gfugrt.us-east-1.rds.amazonaws.com:5432/postgres'
+);
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    avatarURL: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+  }
+);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/index.html"));
@@ -29,22 +59,34 @@ app.use((req, res, next) => {
 });
 
 app.get("/contacts", async (req, res) => {
-  res.send(contacts.defaultData.contacts.map((contact) => ({
-    id: contact.id,
-    name: contact.name,
-    email: contact.email
+  const users = await User.findAll();
+  res.send(users.map(({ dataValues }) => ({
+    id: dataValues.id,
+    name: dataValues.name,
+    email: dataValues.email
   })));
 });
 
-app.delete("/contacts/:id", (req, res) => {
-  res.send(contacts.remove(req.token, req.params.id));
+app.delete("/contacts/:id", async (req, res) => {
+    const {id} = req.params;
+    const deleteItem = await User.destroy({where: {
+        id: id
+    }});
+    res.send(contacts.remove(req.token, req.params.id));
 });
 
-app.post("/contacts", bodyParser.json(), (req, res) => {
+app.post("/contacts", bodyParser.json(), async (req, res) => {
   const { name, email } = req.body;
-
+    const id = Math.random().toString(36).substr(-8);
   if (name && email) {
-    res.send(contacts.add(req.token, req.body));
+      const newUser = await new User({
+          name: name,
+          email: email,
+          id: id
+      })
+      const saveItem = await newUser.save();
+      console.log(saveItem);
+    res.send(contacts.add(req.token, {name: name, email: email, id: id}));
   } else {
     res.status(403).send({
       error: "Please provide both a name and an email address",
@@ -54,9 +96,14 @@ app.post("/contacts", bodyParser.json(), (req, res) => {
 
 app.listen(config.port, async () => {
   try {
+    await sequelize.authenticate();
     console.log("Connection has been established successfully.");
     console.log("Server listening on port %s, Ctrl+C to stop", config.port);
+    await User.sync();
+    contacts.defaultData.contacts.forEach((contact) => {
+      User.findOrCreate({ where: { id: contact.id }, defaults: contact });
+    });
   } catch (error) {
-    console.error("Unable to establish the connection:", error);
+    console.error("Unable to connect to the database:", error);
   }
 });
