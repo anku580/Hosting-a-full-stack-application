22f85c3193a4f681c25017b0a798790ba0841920
anku580 <ankugarg580@gmail.com>
2021-08-30 10:10:14 +0000

    Added env variables for eb using cli

--- reactnd-contacts-server/bin/setup-eb.sh
+++ reactnd-contacts-server/bin/setup-eb.sh
@@ -1,4 +1,5 @@
-set +e
+set -x
+set -e
 
 AWS_CONFIG_FILE=$HOME/.aws/config
 rm -r $HOME/.aws
@@ -10,5 +11,7 @@ echo "[profile eb-cli]" > $HOME/.aws/config
 echo "aws_default_region=$AWS_DEFAULT_REGION" >> $HOME/.aws/config
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> $HOME/.aws/config
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $HOME/.aws/config
+
+eb setenv PORT=$PORT POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_PORT=$POSTGRES_PORT POSTGRES_USERNAME=$POSTGRES_USERNAME
 
 eb deploy workspace-dev
