e502d5c6f355c965f9e73aa63c7898655a88a10f
anku580 <ankugarg580@gmail.com>
2021-08-30 05:17:25 +0000

    Added config files

--- reactnd-contacts-server/bin/setup-eb.sh
+++ reactnd-contacts-server/bin/setup-eb.sh
@@ -1,10 +1,14 @@
-set -x
-set -e
+set +e
 
-mkdir /home/ubuntu/.aws
-touch /home/ubuntu/.aws/config
-chmod 600 /home/ubuntu/.aws/config
-echo "[profile eb-cli]" > /home/ubuntu/.aws/config
-echo "aws_default_region=$AWS_DEFAULT_REGION" >> /home/ubuntu/.aws/config
-echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
-echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
+AWS_CONFIG_FILE=$HOME/.aws/config
+rm -r $HOME/.aws
+mkdir $HOME/.aws
+touch $HOME/.aws/config
+chmod 600 $HOME/.aws/config
+
+echo "[profile eb-cli]" > $HOME/.aws/config
+echo "aws_default_region=$AWS_DEFAULT_REGION" >> $HOME/.aws/config
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> $HOME/.aws/config
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $HOME/.aws/config
+
+eb deploy workspace-dev
